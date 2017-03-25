38ac59c2ba6ca8cc8f4d736e52553c3d2cce79b1
Malcolm <malodita@gmail.com>
2017-03-25 18:27:53 +0000

    Changed model structure to enclose all submodels

--- app/src/main/java/com/malcolm/portsmouthunibus/models/ResponseSchema.java
+++ app/src/main/java/com/malcolm/portsmouthunibus/models/ResponseSchema.java
@@ -12,19 +12,13 @@ import java.util.Map;
 
 
 public class ResponseSchema implements Serializable {
+    private final static long serialVersionUID = 2988541670284738700L;
     @SerializedName("routes")
     private List<Route> routes = null;
     @SerializedName("status")
     private String status;
     private long time;
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-    private final static long serialVersionUID = 2988541670284738700L;
-
-
-
-    public List<Route> getRoutes() {
-        return routes;
-    }
 
     public long getTime() {
         return time;
@@ -34,13 +28,8 @@ public class ResponseSchema implements Serializable {
         this.time = time;
     }
 
-    public Location getLocation() {
-        if (getRoutes().get(0) != null && getRoutes().get(0).getLegs().get(0) != null
-                && getRoutes().get(0).getLegs().get(0).getStartLocation() != null){
-            return getRoutes().get(0).getLegs().get(0).getStartLocation().getLocation();
-        } else {
-            return null;
-        }
+    public List<Route> getRoutes() {
+        return routes;
     }
 
     public String getStatus() {
@@ -57,6 +46,143 @@ public class ResponseSchema implements Serializable {
 
     public void setAdditionalProperty(String name, Object value) {
         this.additionalProperties.put(name, value);
+    }
+
+    public static class Duration{
+        @SerializedName("value")
+        private Double value;
+
+        public Double getValue() {
+            return value;
+        }
+
+        public void setValue(Double value) {
+            this.value = value;
+        }
+    }
+
+    public static class StartLocation{
+        @SerializedName("lat")
+        private Double lat;
+        @SerializedName("lng")
+        private Double lng;
+
+
+        public Location getLocation() {
+            Location location = new Location("Last Location");
+            location.setLatitude(lat);
+            location.setLongitude(lng);
+            return location;
+        }
+    }
+
+    public static class OverviewPolyline{
+        @SerializedName("points")
+        private String points;
+
+
+        public String getPoints() {
+            return points;
+        }
+
+        public void setPoints(String points) {
+            this.points = points;
+        }
+
+
+    }
+
+    public static class Distance{
+        @SerializedName("text")
+        private String text;
+        @SerializedName("value")
+        private Integer value;
+        private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+    }
+
+    public static class Route{
+
+        private List<Leg> legs = null;
+        @SerializedName("overview_polyline")
+        private OverviewPolyline overviewPolyline;
+        @SerializedName("summary")
+        private String summary;
+
+
+        public List<Leg> getLegs() {
+            return legs;
+        }
+
+        public void setLegs(List<Leg> legs) {
+            this.legs = legs;
+        }
+
+        public OverviewPolyline getOverviewPolyline() {
+            return overviewPolyline;
+        }
+
+        public void setOverviewPolyline(OverviewPolyline overviewPolyline) {
+            this.overviewPolyline = overviewPolyline;
+        }
+
+        public String getSummary() {
+            return summary;
+        }
+
+        public void setSummary(String summary) {
+            this.summary = summary;
+        }
+
+    }
+
+    public static class Leg{
+
+        private Distance distance;
+        private Duration duration;
+        @SerializedName("start_location")
+        private StartLocation startLocation;
+        private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+        public StartLocation getStartLocation() {
+            return startLocation;
+        }
+
+        public void setStartLocation(StartLocation startLocation) {
+            this.startLocation = startLocation;
+        }
+
+        public Distance getDistance() {
+            return distance;
+        }
+
+        public void setDistance(Distance distance) {
+            this.distance = distance;
+        }
+
+        public Duration getDuration() {
+            return duration;
+        }
+
+        public void setDuration(Duration duration) {
+            this.duration = duration;
+        }
+
     }
 
 
