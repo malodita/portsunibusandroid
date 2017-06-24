ecd525d0b2551ae389502a6a9c4f794da8429723
Malcolm <malodita@gmail.com>
2017-06-24 08:09:03 +0100

    Moved click listener back into the ViewHolder

--- app/src/main/java/com/malcolm/portsmouthunibus/adapters/TimetableFragmentAdapter.java
+++ app/src/main/java/com/malcolm/portsmouthunibus/adapters/TimetableFragmentAdapter.java
@@ -1,19 +1,12 @@
 package com.malcolm.portsmouthunibus.adapters;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.malcolm.portsmouthunibus.R;
-import com.malcolm.portsmouthunibus.detail.DetailActivity;
 import com.malcolm.portsmouthunibus.viewholders.TimetableItemViewHolder;
 import com.malcolm.unibusutilities.Times;
 
@@ -29,12 +22,10 @@ public class TimetableFragmentAdapter extends RecyclerView.Adapter<TimetableItem
     private List<Times> times = Collections.emptyList();
     private LayoutInflater inflater;
     private Context context;
-    private FirebaseAnalytics analytics;
     private int currentStopId;
 
     public TimetableFragmentAdapter(Context context, ArrayList<Times> times, int currentStopId){
         this.context = context;
-        analytics = FirebaseAnalytics.getInstance(context);
         inflater = LayoutInflater.from(context);
         this.times = times;
         this.currentStopId = currentStopId;
@@ -52,34 +43,10 @@ public class TimetableFragmentAdapter extends RecyclerView.Adapter<TimetableItem
 
     @Override
     public void onBindViewHolder(final TimetableItemViewHolder holder, int position) {
-        final int holderPosition = position;
-        holder.time.setText(times.get(holderPosition).getTime());
-        holder.destination.setText(times.get(holderPosition).getDestination());
-        holder.position = times.get(holderPosition).getId();
-        holder.layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(context, DetailActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putInt("listPosition", holderPosition);
-                bundle.putString("stop", holder.destination.getText().toString());
-                bundle.putString("time", holder.time.getText().toString());
-                i.putExtra(context.getString(R.string.intent_list_position), holder.position);
-                i.putExtra(context.getString(R.string.intent_stop), holder.destination.getText());
-                i.putExtra(context.getString(R.string.intent_stop_time), holder.time.getText());
-                i.putExtra(context.getString(R.string.intent_stop_viewed), currentStopId);
-                analytics.logEvent(context.getString(R.string.firebase_timetable_detail_request), bundle);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    ActivityOptionsCompat options = ActivityOptionsCompat.makeClipRevealAnimation(v, 0, 0
-                            , v.getMeasuredWidth(), v.getMeasuredHeight());
-                    context.startActivity(i, options.toBundle());
-                } else {
-                    context.startActivity(i);
-                    Activity activity = (Activity) context;
-                    activity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                }
-            }
-        });
+        holder.time.setText(times.get(position).getTime());
+        holder.destination.setText(times.get(position).getDestination());
+        holder.position = times.get(position).getId();
+        holder.currentStopId = this.currentStopId;
     }
 
 
--- app/src/main/java/com/malcolm/portsmouthunibus/viewholders/TimetableItemViewHolder.java
+++ app/src/main/java/com/malcolm/portsmouthunibus/viewholders/TimetableItemViewHolder.java
@@ -1,25 +1,62 @@
 package com.malcolm.portsmouthunibus.viewholders;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.malcolm.portsmouthunibus.R;
+import com.malcolm.portsmouthunibus.detail.DetailActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class TimetableItemViewHolder extends RecyclerView.ViewHolder{
+public class TimetableItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
     @BindView(R.id.time) public TextView time;
     @BindView(R.id.destination) public TextView destination;
     @BindView(R.id.layout) public ConstraintLayout layout;
     public int position;
+    public int currentStopId;
+    private Context context;
+    private FirebaseAnalytics analytics;
+
 
 
     public TimetableItemViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
+        context = itemView.getContext();
+        analytics = FirebaseAnalytics.getInstance(context);
+        itemView.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(context, DetailActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt("listPosition", getAdapterPosition());
+        bundle.putString("stop", destination.getText().toString());
+        bundle.putString("time", time.getText().toString());
+        i.putExtra(context.getString(R.string.intent_list_position), position);
+        i.putExtra(context.getString(R.string.intent_stop), destination.getText());
+        i.putExtra(context.getString(R.string.intent_stop_time), time.getText());
+        i.putExtra(context.getString(R.string.intent_stop_viewed), currentStopId);
+        analytics.logEvent(context.getString(R.string.firebase_timetable_detail_request), bundle);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityOptionsCompat options = ActivityOptionsCompat.makeClipRevealAnimation(view, 0, 0
+                    , view.getMeasuredWidth(), view.getMeasuredHeight());
+            context.startActivity(i, options.toBundle());
+        } else {
+            context.startActivity(i);
+            Activity activity = (Activity) context;
+            activity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        }
+    }
 }
