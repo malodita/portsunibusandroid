e6093c39d418861f539841a271883ae413ace84b
Malcolm <malodita@gmail.com>
2017-07-23 11:08:46 +0100

    Addition of completed ImageGenerator

--- app/src/main/java/com/malcolm/portsmouthunibus/utilities/ImageGenerator.java
+++ app/src/main/java/com/malcolm/portsmouthunibus/utilities/ImageGenerator.java
@@ -13,14 +13,79 @@ public final class ImageGenerator {
     public static Drawable generateImage(Context context, int stop){
         int nightMode = context.getResources().getConfiguration().uiMode
                 & Configuration.UI_MODE_NIGHT_MASK;
-        String item;
-        String[] stops = context.getResources().getStringArray(R.array.bus_stops_spinner);
+        Drawable drawable;
         if (nightMode == Configuration.UI_MODE_NIGHT_YES){
-            item = "R.drawable." + stops[stop] + "_night";
+            drawable = chooseNightPicture(context, stop);
         } else {
-            item = "R.drawable." + stops[stop];
+            drawable = chooseDayPicture(context, stop);
         }
-        return ContextCompat.getDrawable(context, context.getResources().getIdentifier(item, "drawable", context.getPackageName()));
+        if (drawable != null){
+            return drawable;
+        } else {
+            if (nightMode == Configuration.UI_MODE_NIGHT_YES) {
+                return ContextCompat.getDrawable(context, R.drawable.generic_night);
+            } else {
+               return  ContextCompat.getDrawable(context,R.drawable.generic_day);
+            }
+        }
+    }
+
+    private static Drawable chooseNightPicture(Context context, int stop){
+        switch (stop){
+            case 1:
+                return ContextCompat.getDrawable(context,R.drawable.eastney_night);
+            case 2:
+                return ContextCompat.getDrawable(context, R.drawable.langstone_night);
+            case 3:
+                return ContextCompat.getDrawable(context, R.drawable.locksway_night);
+            case 4:
+                return ContextCompat.getDrawable(context, R.drawable.adj_lidl_night);
+            case 5:
+                return ContextCompat.getDrawable(context, R.drawable.fawcett_night);
+            case 6:
+                return ContextCompat.getDrawable(context, R.drawable.ibis_night);
+            case 8:
+                return ContextCompat.getDrawable(context, R.drawable.nuffield_night);
+            case 9:
+                return ContextCompat.getDrawable(context, R.drawable.law_night);
+            case 10:
+                return ContextCompat.getDrawable(context, R.drawable.fratton_night);
+            case 11:
+                return ContextCompat.getDrawable(context, R.drawable.opp_lidl_night);
+            case 12:
+                return ContextCompat.getDrawable(context, R.drawable.milton_night);
+            default:
+                return ContextCompat.getDrawable(context, R.drawable.generic_night);
+        }
+    }
+
+    private static Drawable chooseDayPicture(Context context, int stop){
+        switch (stop){
+            case 1:
+                return ContextCompat.getDrawable(context,R.drawable.eastney);
+            case 2:
+                return ContextCompat.getDrawable(context, R.drawable.langstone);
+            case 3:
+                return ContextCompat.getDrawable(context, R.drawable.locksway);
+            case 4:
+                return ContextCompat.getDrawable(context, R.drawable.adj_lidl);
+            case 5:
+                return ContextCompat.getDrawable(context, R.drawable.fawcett);
+            case 6:
+                return ContextCompat.getDrawable(context, R.drawable.ibis);
+            case 8:
+                return ContextCompat.getDrawable(context, R.drawable.nuffield);
+            case 9:
+                return ContextCompat.getDrawable(context, R.drawable.law);
+            case 10:
+                return ContextCompat.getDrawable(context, R.drawable.fratton);
+            case 11:
+                return ContextCompat.getDrawable(context, R.drawable.opp_lidl);
+            case 12:
+                return ContextCompat.getDrawable(context, R.drawable.milton);
+            default:
+                return ContextCompat.getDrawable(context, R.drawable.generic_day);
+        }
     }
 
 }
